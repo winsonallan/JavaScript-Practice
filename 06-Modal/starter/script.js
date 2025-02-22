ec53eba27e18174152b97221fa8ed7a6cfa10cf7
winsonallan <winsonallanw@gmail.com>
2025-02-22 12:37:20 +0700

    Modal done

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const showModalElement = document.querySelectorAll('.show-modal');
+const modalElement = document.querySelector('.modal');
+const overlayElement = document.querySelector('.overlay');
+const closeModal = () => {
+  modalElement.classList.add('hidden');
+  overlayElement.classList.add('hidden');
+};
+const openModal = () => {
+  modalElement.classList.remove('hidden');
+  overlayElement.classList.remove('hidden');
+};
+
+showModalElement.forEach(element => {
+  element.addEventListener('click', openModal);
+});
+
+overlayElement.addEventListener('click', closeModal);
+document.querySelector('.close-modal').addEventListener('click', closeModal);
