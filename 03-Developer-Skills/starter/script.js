b917107c6e6244e6b0916f2fd9378310441e6403
winsonallan <winsonallanw@gmail.com>
2025-02-22 09:43:51 +0700

    before section 7

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -4,4 +4,35 @@
 const x = '23';
 const calcAge = birthYear => 2037 - birthYear;
 
-console.log(calcAge(1991));
+console.log(calcAge(2000));
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celcius',
+    // value: Number(prompt('Degrees celcius: ')),
+    value: 10,
+  };
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+console.log(measureKelvin());
+
+// There are console.log(), console.warn(), console.eror(), console.table()
+
+//CODING CHALLENGE 1
+const printForecast = function (arr) {
+  let forecastString = '';
+  for (let i = 0; i < arr.length; i++) {
+    forecastString += `...${arr[i]} °C in ${i + 1} days`;
+  }
+  forecastString += `...`;
+  console.log(forecastString);
+};
+const forecastArr = [17, 21, 23];
+printForecast(forecastArr);
+
+const forecastArr2 = [12, 5, -5, 0, 4];
+printForecast(forecastArr2);
