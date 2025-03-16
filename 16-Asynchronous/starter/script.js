a1e947e19960c4dd8ca8ecfa1da6dfb356ccc10e
winsonallan <winsonallanw@gmail.com>
2025-03-16 16:59:59 +0700

    asynchronous done

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -5,8 +5,460 @@ const countriesContainer = document.querySelector('.countries');
 
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
 // https://restcountries.com/v2/name/portugal
+// https://countries-api-836d.onrender.com/countries/
 
 // NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+
+// Old School Method
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open(
+//     'GET',
+//     `https://countries-api-836d.onrender.com/countries/name/${country}`,
+//   );
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+
+//     const html = `
+//         <article class="country">
+//             <img class="country__img" src="${data.flag}" />
+//             <div class="country__data">
+//             <h3 class="country__name">${data.name}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(
+//               +data.population / 1000000
+//             ).toFixed(1)} people</p>
+//             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//             <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//             </div>
+//         </article>
+//       `;
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// getCountryData('portugal');
+// getCountryData('usa');
+// getCountryData('indonesia');
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  //   countriesContainer.style.opacity = 1;
+};
+
+const renderCountry = function (data, className = '') {
+  const html = `
+        <article class="country ${className}">
+            <img class="country__img" src="${data.flag}" />
+            <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+            </div>
+        </article>
+      `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  const request = new XMLHttpRequest();
+  request.open(
+    'GET',
+    `https://countries-api-836d.onrender.com/countries/name/${country}`,
+  );
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const [neighbour] = data.borders;
+    console.log(neighbour);
+    if (!neighbour) return;
+
+    const request2 = new XMLHttpRequest();
+    request2.open(
+      'GET',
+      `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`,
+    );
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      //   console.log(this.responseText);
+
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+// getCountryAndNeighbour('usa');
+
+// https://countries-api-836d.onrender.com/countries/
+// const request = fetch(
+//   'https://countries-api-836d.onrender.com/countries/name/portugal',
+// );
+// console.log(request);
+
+// const getCountryData = function (country) {
+//   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+//     .then(function (response) {
+//       //   console.log(response);
+
+//       if (!response.ok)
+//         throw new Error(`Country not found (${response.status})`);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       //   console.log(data);
+//       renderCountry(data[0]);
+
+//       const neighbour = data[0].borders?.[0];
+
+//       if (!neighbour) return;
+
+//       return fetch(
+//         `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`,
+//       );
+//     })
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(data => renderCountry(data, 'neighbour'))
+//     .catch(err => {
+//       console.log(`${err} 🔥`);
+//       renderError(`Something went wrong ${err.message}`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+const getJSON = function (url, errorMsg = 'Something Went Wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} ${response.status}`);
+
+    return response.json();
+  });
+};
+
+// const getCountryData = function (country) {
+//   getJSON(
+//     `https://countries-api-836d.onrender.com/countries/name/${country}`,
+//     'Country not found',
+//   )
+//     .then(function (data) {
+//       //   console.log(data);
+//       renderCountry(data[0]);
+
+//       const neighbour = data[0].borders?.[0];
+
+//       if (!neighbour) throw new Error('No neighbour found!');
+
+//       return getJSON(
+//         `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`,
+//         'Country Not Found!',
+//       );
+//     })
+//     .then(data => renderCountry(data, 'neighbour'))
+//     .catch(err => {
+//       console.log(`${err} 🔥`);
+//       renderError(`Something went wrong ${err.message}`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+// btn.addEventListener('click', function () {
+//   getCountryData('portugal');
+// });
+
+// console.log('boom', btn);
+
+// // getCountryData('australia');
+
+// console.log('Test start');
+// setTimeout(() => console.log('0 sec timer'), 0);
+// Promise.resolve('Resoled promise 1').then(res => console.log(res));
+
+// Promise.resolve('Resolved promise 2').then(res => {
+//   for (let i = 0; i < 10000000; i++) {}
+//   console.log(res);
+// });
+
+// console.log('Test end');
+
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   console.log('Lottery draw is happening');
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       resolve('You Win!');
+//     } else {
+//       reject(new Error(`You lost your money!`));
+//     }
+//   });
+// });
+
+// lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// const wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
+
+// wait(1)
+//   .then(() => {
+//     console.log('1 second passed');
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log('2 second passed');
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log('3 second passed');
+//     return wait(1);
+//   })
+//   .then(() => console.log('4 second passed'));
+
+// Promise.resolve('abc').then(x => console.log(x));
+// Promise.reject(new Error('Problem!')).catch(x => console.error(x));
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err),
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+getPosition().then(pos => console.log(pos));
+
+const fetchAlpha = function (data) {
+  return fetch(
+    `https://countries-api-836d.onrender.com/countries/alpha/${data}`,
+  ).then(response => {
+    console.log(response);
+    return response.json();
+  });
+};
+
+// const whereAmI = function () {
+//   getPosition()
+//     .then(pos => {
+//       const { latitude: lat, longitude: lng } = pos.coords;
+
+//       return fetch(
+//         `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`,
+//       );
+//     })
+//     .then(res => {
+//       if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+//       return res.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       // renderCountry(data);
+//       const countryName = data.countryName;
+//       console.log(countryName);
+//       return fetch(
+//         `https://countries-api-836d.onrender.com/countries/name/${countryName}`,
+//       );
+//     })
+//     .then(res => {
+//       if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+//       return res.json();
+//     })
+//     .then(data => {
+//       console.log(data);
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders;
+
+//       if (!neighbour) return;
+
+//       return neighbour;
+//     })
+//     .then(data => {
+//       console.log('hopefully neighbors', data);
+
+//       const neighborsData = data.map(d => fetchAlpha(d));
+//       return Promise.all(neighborsData);
+//     })
+//     .then(function (response) {
+//       console.log(response);
+//       response.forEach(element => {
+//         renderCountry(element, 'neighbour');
+//       });
+//     })
+//     .catch(err => {
+//       console.log(`${err} 🔥`);
+//       renderError(`Something went wrong ${err.message}`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+// btn.addEventListener('click', whereAmI);
+
+const whereAmIAsync = async function () {
+  try {
+    const pos = await getPosition();
+    const { latitude: lat, longitude: lng } = pos.coords;
+
+    const resGeo = await fetch(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`,
+    );
+    const dataGeo = await resGeo.json();
+
+    // console.log(dataGeo);
+
+    const res = await fetch(
+      `https://countries-api-836d.onrender.com/countries/name/${dataGeo.countryName}`,
+    );
+    const data = await res.json();
+
+    // console.log(data);
+
+    renderCountry(data[0]);
+
+    const borderCountries = data[0].borders ? data[0].borders : null;
+
+    if (!borderCountries) return;
+    const neighborsData = await Promise.all(borderCountries.map(fetchAlpha));
+
+    // console.log(neighborsData);
+
+    neighborsData.forEach(element => {
+      renderCountry(element, 'neighbour');
+    });
+
+    return `You are in ${dataGeo.countryName}`;
+  } catch (err) {
+    console.log(err);
+    renderError('Something went wrong' + err.message);
+
+    // Reject promise returned form async function
+    throw err;
+  }
+};
+
+// whereAmIAsync()
+//   .then(city => console.log(city))
+//   .catch(err => console.error(err.message))
+//   .finally(() => console.log('Finished getting location'));
+
+(async function () {
+  try {
+    const currentCountry = await whereAmIAsync();
+    console.log(currentCountry);
+  } catch (err) {
+    console.error(err.message);
+  }
+
+  console.log('Finished getting location');
+})();
+
+// try {
+//   let y = 1;
+//   const x = 2;
+//   x = 3;
+// } catch (err) {
+//   alert(err.message);
+// }
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    const [data1] = await getJSON(
+      `https://countries-api-836d.onrender.com/countries/name/${c1}`,
+    );
+
+    const [data2] = await getJSON(
+      `https://countries-api-836d.onrender.com/countries/name/${c2}`,
+    );
+
+    const [data3] = await getJSON(
+      `https://countries-api-836d.onrender.com/countries/name/${c3}`,
+    );
+
+    const data = await Promise.all([
+      getJSON(`https://countries-api-836d.onrender.com/countries/name/${c1}`),
+      getJSON(`https://countries-api-836d.onrender.com/countries/name/${c2}`),
+      getJSON(`https://countries-api-836d.onrender.com/countries/name/${c3}`),
+    ]);
+
+    console.log([data1.capital, data2.capital, data3.capital]);
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('portugal', 'canada', 'tanzania');
+
+// Promise.race() -> Returns only first settled promise, whether resolved or rejected
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://countries-api-836d.onrender.com/countries/name/italy`),
+    getJSON(`https://countries-api-836d.onrender.com/countries/name/china`),
+    getJSON(`https://countries-api-836d.onrender.com/countries/name/portugal`),
+  ]);
+
+  console.log(res[0]);
+})();
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('Request took too long!'));
+    }, s * 1000);
+  });
+};
+
+Promise.race([
+  getJSON(`https://countries-api-836d.onrender.com/countries/name/italy`),
+  timeout(10),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+// Promise.allSettled -> Never shortcircuits, returns all settled promises
+
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
+
+// Promise.any -> Returned first FULFILLED (resolved) promise
+Promise.any([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
